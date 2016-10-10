c801d28f3e00a6ea413700213bdb8c6e159abbb5
Dody Agung Saputro <hello@dodyagung.com>
2016-10-10 22:58:24 +0700

    add citcuit status link

--- resources/views/api/@nav_bottom.blade.php
+++ resources/views/api/@nav_bottom.blade.php
@@ -1,5 +1,6 @@
-<a href="#top">[&uarr;] Top</a> | 
+<a href="#top">[&uarr;] Top</a> |
 <a href="{{ url('trends') }}">Trends</a> |
-<a href="{{ url('settings') }}">Settings</a> | 
-<a href="{{ url('about') }}">About</a> | 
+<a href="{{ url('settings') }}">Settings</a> |
+<a href="{{ url('about') }}">About</a> |
+<a href="http://status.citcuit.in" target="_blank">Status</a> |
 <a href="{{ url('signout') }}">Sign Out</a>
--- resources/views/non_api/@nav_top.blade.php
+++ resources/views/non_api/@nav_top.blade.php
@@ -1,2 +1,3 @@
 <strong><a href="{{ url('') }}">Home</a></strong> |
-<a href="{{ url('about') }}">About</a>
+<a href="{{ url('about') }}">About</a> |
+<a href="http://status.citcuit.in" target="_blank">Status</a>
