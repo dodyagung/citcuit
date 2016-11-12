d6a2a457786682c3ac71dc5c42985d91cad8a133
Dody Agung Saputro <hello@dodyagung.com>
2016-11-12 22:49:02 +0700

    add upload image from remote files (2) - sorry

--- resources/views/api/upload.blade.php
+++ resources/views/api/upload.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
-@section('title', 'Upload')
+@section('title', 'Upload Image')
 
 @section('content')
 <nav class="sub-menu">
@@ -30,6 +30,29 @@
         <input type="file" name="image3">
         Image 4 (optional) :<br />
         <input type="file" name="image4">
+        Text :
+        <textarea id="status" name="tweet" required></textarea>
+        @if (session('auth.facebook_token'))
+        <label><input type="checkbox" name="fb" id="fb" value="yes"> Share to Facebook</label><br />
+        @else
+        <a href="{{ url('settings/facebook') }}">Share to Facebook</a><br />
+        @endif
+        {{ csrf_field() }}
+        <button type="submit">Tweet</button>
+    </form>
+    <hr />
+    <strong>Or, if you want to upload via the image URL :</strong><br />
+    <em>Example : http://www.w3schools.com/images/colorpicker.gif</em><br />
+    <br />
+    <form method="POST" action="{{ url('upload/remote') }}">
+        Image 1 (required) :<br />
+        <input type="text" name="image1" required>
+        Image 2 (optional) :<br />
+        <input type="text" name="image2">
+        Image 3 (optional) :<br />
+        <input type="text" name="image3">
+        Image 4 (optional) :<br />
+        <input type="text" name="image4">
         Text :
         <textarea id="status" name="tweet" required></textarea>
         @if (session('auth.facebook_token'))
