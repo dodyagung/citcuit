8fcc2703f236d1d2226838b67fc473876b6ad636
Dody Agung Saputro <hello@dodyagung.com>
2016-11-19 22:16:50 +0700

    rearrange menu

--- resources/views/api/@nav_bottom.blade.php
+++ resources/views/api/@nav_bottom.blade.php
@@ -1,6 +1,8 @@
-<strong>{{ $global_time }}</strong> | 
+<strong>{{ $global_time }}</strong> |
 <a href="#top">[&uarr;] Top</a> |
-<a href="{{ url('trends') }}">Trends</a> |
+<a href="{{ url('following/' . session('auth.screen_name')) }}">Following</a> |
+<a href="{{ url('followers/' . session('auth.screen_name')) }}">Followers</a> |
+<a href="{{ url('likes/' . session('auth.screen_name')) }}">Likes</a> |
 <a href="{{ url('settings') }}">Settings</a> |
 <a href="{{ url('about') }}">About</a> |
 <a href="http://status.citcuit.in" target="_blank">Status</a> |
