db34ca7499f4f2aee13ebf6f17015192dbc9bf2b
Dody Agung Saputro <hello@dodyagung.com>
2017-05-01 13:47:43 +0700

    fix favicon and add touch icon

--- resources/views/api/@tweet.blade.php
+++ resources/views/api/@tweet.blade.php
@@ -1,5 +1,5 @@
 <form method="POST" action="{{ url('tweet') }}">
-    <img src="{{ url('assets/img/favicon.png') }}" /> What's happening?
+    <img src="{{ url('favicon.ico') }}" /> What's happening?
     <textarea id="status" name="tweet" required>@if (isset($screen_name)){{ $screen_name }}@endif</textarea>
     @if (session('auth.facebook_token'))
     <label><input type="checkbox" name="fb" id="fb" value="yes"> Share to Facebook</label><br />
