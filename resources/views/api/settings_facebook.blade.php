b455b01d20efe93aeb97d1a0961b4d2efc348b2c
Dody Agung S <dodyags@gmail.com>
2018-06-20 21:52:43 +0700

    disable facebook connect due to facebook new policy

--- resources/views/api/settings_facebook.blade.php
+++ resources/views/api/settings_facebook.blade.php
@@ -9,7 +9,10 @@
     <a href="{{ url('settings') }}">&laquo; Back to Settings</a>
 </section>
 <section>
-    @if ($logged_in)
+    Facebook Connect is currently unavailable due to Facebook new policy updates that doesn't allow posting without being done by the user itself.<br /><br />
+
+    See <a target="_blank" href="https://developers.facebook.com/blog/post/2018/04/24/new-facebook-platform-product-changes-policy-updates/">https://developers.facebook.com/blog/post/2018/04/24/new-facebook-platform-product-changes-policy-updates/</a>
+    <!-- @if ($logged_in)
     Congratulations, your account is connected to Facebook!<br />
     <br />
     Facebook ID : {{ $user->getID() }}<br />
@@ -20,6 +23,6 @@
     Connect to Facebook and share your status to Twitter and Facebook in single click.<br />
     <br />
     <a href="{{ url('settings/facebook/login') }}"><img src="{{ url('assets/img/fb.png') }}" width="100" alt="Facebook Connect" /></a>
-    @endif
+    @endif -->
 </section>
 @endsection
