8f9b52a435be5d5591ccbecdd45d514189ab49aa
Dody Agung Saputro <dodyags@gmail.com>
2021-01-09 18:23:44 +0700

    add signin link and update homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,10 +11,11 @@
     Secure, slim, fast, lightweight, no ads, no database and of course we're <a
         href="https://github.com/dodyagung/CitCuit" target="_blank">open-sourced</a>!<br>
     <br>
-    Built with <a href="https://laravel.com" target="_blank">Laravel</a>. Made with &hearts; in Jakarta,
-    Indonesia.<br>
+    Built with <a href="https://laravel.com" target="_blank">Laravel</a>. Made with &hearts; by <a
+        href="https://twitter.com/dodyags" target="_blank">@dodyags</a> in Jakarta,
+    Indonesia.
     <br>
-    <a href="{{ url('signin') }}">
+    <a href="{{ url('auth/login') }}">
         <img src="{{ url('assets/img/signin.png') }}" alt="Sign in with Twitter" />
     </a><br>
     Don't have a Twitter account? <a href="https://mobile.twitter.com/i/flow/signup" target="_blank">Sign up</a>.<br>
