b69da3251b015cd9b00b0ec82794c92edfeedf2f
Dody Agung Saputro <dodyags@gmail.com>
2021-01-09 16:53:43 +0700

    add home and about controoler and route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", "HomeController@index")->name("home");
+Route::get("about", "AboutController@index")->name("about");
