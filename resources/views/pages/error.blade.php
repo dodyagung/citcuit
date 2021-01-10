20134495cd66ea0f37085b0b467459408ac263e3
Dody Agung Saputro <dodyags@gmail.com>
2021-01-10 11:09:27 +0700

    add log message (if any)

--- resources/views/pages/error.blade.php
+++ resources/views/pages/error.blade.php
@@ -5,6 +5,7 @@
     <div class="alert alert-error">
         <strong>@yield('code') @yield('message')</strong>
         <br />
+        @if($exception->getMessage()) {{ $exception->getMessage() }}<br>@endif
         <br />
         If often, try to <a href="{{ url('signout') }}">signout</a> then signin again, or <a
             href="{{ url('about#contribute') }}">contact us</a>.
