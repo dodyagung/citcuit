265156a172ca1e41a7412c11532cc7ab38bc37f9
Dody Agung Saputro <dodyags@gmail.com>
2021-01-10 20:43:45 +0700

    fix github url

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,7 @@
     CitCuit is a mobile Twitter client, alternative of official <a href="https://mobile.twitter.com"
         target="_blank">mobile.twitter.com</a> website.<br>
     Secure, slim, fast, lightweight, no ads, no database and of course we're <a
-        href="https://github.com/dodyagung/CitCuit" target="_blank">open-sourced</a>!<br>
+        href="https://github.com/dodyagung/citcuit" target="_blank">open-sourced</a>!<br>
     <br>
     Built with <a href="https://laravel.com" target="_blank">Laravel</a>. Made with &hearts; by <a
         href="https://twitter.com/dodyags" target="_blank">@dodyags</a> in Jakarta, Indonesia.
