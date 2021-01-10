08d0b395736a589ae67587653fbc704ac7544fd7
Dody Agung Saputro <dodyags@gmail.com>
2021-01-10 11:09:04 +0700

    change log from single to daily

--- config/logging.php
+++ config/logging.php
@@ -5,7 +5,6 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Log Channel
@@ -17,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    "default" => env("LOG_CHANNEL", "stack"),
 
     /*
     |--------------------------------------------------------------------------
@@ -34,71 +33,70 @@ return [
     |
     */
 
-    'channels' => [
-        'stack' => [
-            'driver' => 'stack',
-            'channels' => ['single'],
-            'ignore_exceptions' => false,
+    "channels" => [
+        "stack" => [
+            "driver" => "stack",
+            "channels" => ["daily"],
+            "ignore_exceptions" => false,
         ],
 
-        'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
+        "single" => [
+            "driver" => "single",
+            "path" => storage_path("logs/laravel.log"),
+            "level" => "debug",
         ],
 
-        'daily' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
-            'days' => 14,
+        "daily" => [
+            "driver" => "daily",
+            "path" => storage_path("logs/laravel.log"),
+            "level" => "debug",
+            "days" => 14,
         ],
 
-        'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
+        "slack" => [
+            "driver" => "slack",
+            "url" => env("LOG_SLACK_WEBHOOK_URL"),
+            "username" => "Laravel Log",
+            "emoji" => ":boom:",
+            "level" => "critical",
         ],
 
-        'papertrail' => [
-            'driver' => 'monolog',
-            'level' => 'debug',
-            'handler' => SyslogUdpHandler::class,
-            'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
+        "papertrail" => [
+            "driver" => "monolog",
+            "level" => "debug",
+            "handler" => SyslogUdpHandler::class,
+            "handler_with" => [
+                "host" => env("PAPERTRAIL_URL"),
+                "port" => env("PAPERTRAIL_PORT"),
             ],
         ],
 
-        'stderr' => [
-            'driver' => 'monolog',
-            'handler' => StreamHandler::class,
-            'formatter' => env('LOG_STDERR_FORMATTER'),
-            'with' => [
-                'stream' => 'php://stderr',
+        "stderr" => [
+            "driver" => "monolog",
+            "handler" => StreamHandler::class,
+            "formatter" => env("LOG_STDERR_FORMATTER"),
+            "with" => [
+                "stream" => "php://stderr",
             ],
         ],
 
-        'syslog' => [
-            'driver' => 'syslog',
-            'level' => 'debug',
+        "syslog" => [
+            "driver" => "syslog",
+            "level" => "debug",
         ],
 
-        'errorlog' => [
-            'driver' => 'errorlog',
-            'level' => 'debug',
+        "errorlog" => [
+            "driver" => "errorlog",
+            "level" => "debug",
         ],
 
-        'null' => [
-            'driver' => 'monolog',
-            'handler' => NullHandler::class,
+        "null" => [
+            "driver" => "monolog",
+            "handler" => NullHandler::class,
         ],
 
-        'emergency' => [
-            'path' => storage_path('logs/laravel.log'),
+        "emergency" => [
+            "path" => storage_path("logs/laravel.log"),
         ],
     ],
-
 ];
