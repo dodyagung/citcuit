084c81626988a8d9664c3efffbd6ac33a8b5be61
Dody Agung Saputro <dodyags@gmail.com>
2021-01-10 11:59:03 +0700

    add username at header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,8 @@
 {{-- @if (session('auth'))
 <a href="{{ url('user/' . session('auth.screen_name')) }}">{{ '@' . session('auth.screen_name') }}</a> -
 @endif --}}
+@if(session()->has("access_token"))
+<a href="{{ url('user/' . session()->get("access_token.screen_name")) }}">
+    {{ '@' . session()->get("access_token.screen_name") }}</a> -
+@endif
 CitCuit
