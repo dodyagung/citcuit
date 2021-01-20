9b51edda9a5b68a635329a2aa1a9df90bc8264f1
Dody Agung Saputro <dodyags@gmail.com>
2021-01-20 08:27:20 +0700

    fix CVE-2021-21263

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 6.20.9.
+ * Generated for Laravel 6.20.13.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
